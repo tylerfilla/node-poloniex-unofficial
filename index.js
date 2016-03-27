a3232b59360b3da3f2884289249b4977d4b0c62c
Tyler Filla <tylerfilla@gmail.com>
2016-03-27 01:21:19 -0500

    Updated package.json and added index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+
+/*
+ *
+ * node-poloniex-unofficial
+ *
+ * An unofficial wrapper for the Poloniex cryptocurrency exchange API written
+ * for Node.js
+ *
+ */
+
+// Import modules
+var crypto = require("crypto");
+
+// TODO: Stuff goes here
+var poloniex = {};
+
+// Export module
+module.export = poloniex;
+
