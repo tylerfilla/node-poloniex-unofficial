f60a6c790ae0f2ecfd5bc4afc35bcf4c7d650dc2
Tyler Filla <tylerfilla@gmail.com>
2016-03-28 18:47:34 -0500

    Proliferated MIT license notice

--- examples/pushTrollbox.js
+++ examples/pushTrollbox.js
@@ -3,8 +3,13 @@
  *
  * poloniex-unofficial
  *
- * Yet another unofficial wrapper for the Poloniex cryptocurrency exchange APIs
- * on Node.js
+ * Yet another unofficial Node.js wrapper for the Poloniex cryptocurrency
+ * exchange APIs.
+ *
+ * Copyright (c) 2016 Tyler Filla
+ *
+ * This software may be modified and distributed under the terms of the MIT
+ * license. See the LICENSE file for details.
  *
  */
 
