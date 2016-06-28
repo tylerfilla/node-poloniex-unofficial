9a36f427c88f3f8f4aa37e67ca52721ff9e07772
Tyler Filla <tylerfilla@gmail.com>
2016-06-28 16:21:19 -0500

    Add example file for order book tracking

---
+++ examples/orderbook.js
@@ -0,0 +1,27 @@
+
+/*
+ *
+ * poloniex-unofficial
+ * https://git.io/polonode
+ *
+ * Yet another unofficial Node.js wrapper for the Poloniex cryptocurrency
+ * exchange APIs.
+ *
+ * Copyright (c) 2016 Tyler Filla
+ *
+ * This software may be modified and distributed under the terms of the MIT
+ * license. See the LICENSE file for details.
+ *
+ */
+
+// Import modules
+var polo = require("./../../");
+
+// Create a new order book tracker for the bitcoin-monero market
+var book = new polo.OrderBook("BTC_XMR");
+
+// Create and use default wrappers
+book.useDefaultWrappers();
+
+// Start tracking the market
+book.start();
